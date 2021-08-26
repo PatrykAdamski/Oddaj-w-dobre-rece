74c2570b04254d9bd0fd2cea6acdbe0507c99d51
PatrykAdamski <patada7090@gmail.com>
2021-08-26 11:03:49 +0200

    navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <p>Lorem asdn asmadi njas ndn asjdnasjdnas</p>
-    </div>
+    <Router>
+      <div className = "nav nav__main">
+        <div className = "nav__main--element nav__main--element--different">
+          <Link className='link' to="/">Zaloguj</Link>
+        </div>
+        <div className = "nav__main--element nav__main--element--different">
+          <Link className='link' to="/">Załóż konto</Link>
+        </div>
+      </div>
+       <nav className = "nav nav__main">
+          <ul className = "nav__main--list">
+            <li className = "nav__main--element">
+              <Link className='link' to="/">Start</Link>
+            </li>
+            <li className = "nav__main--element">
+              <Link className='link' to="/">O co chodzi?</Link>
+            </li>
+            <li className = "nav__main--element">
+              <Link className='link' to="/">O nas</Link>
+            </li>
+            <li className = "nav__main--element">
+              <Link className='link' to="/">Fundacja i organizacje</Link>
+            </li>
+            <li className = "nav__main--element">
+              <Link className='link' to="/">Kontakt</Link>
+            </li>
+          </ul>
+        </nav>
+      <Switch>
+
+      </Switch>
+    </Router>
   );
 }
 
