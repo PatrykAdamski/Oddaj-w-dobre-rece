e11d9bb32ea3005d179451da61be9d21cd27648c
PatrykAdamski <patada7090@gmail.com>
2021-10-02 04:05:24 +0200

    Create step thank you in form hand over stuff

---
+++ src/components/FormHandOverStuffStepThankYou.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function FormHandOverStuffStepThankYou() {
+  return (
+    <div className="stepThankYou">
+      <h3 className="stepThankYou__title">
+        Dziękujemy za przesłanie formularza Na maila prześlemy wszelkie
+        informacje o odbiorze.
+      </h3>
+      <div className="ornament"></div>
+    </div>
+  );
+}
+
+export default FormHandOverStuffStepThankYou;
--- src/components/FormHandOverStuffSteps.js
+++ src/components/FormHandOverStuffSteps.js
@@ -2,6 +2,7 @@ import React from "react";
 import FormHandOverStuffStep1 from "./FormHandOverStuffStep1";
 import FormHandOverStuffStep2 from "./FormHandOverStuffStep2";
 import FormHandOverStuffStep3 from "./FormHandOverStuffStep3";
+import FormHandOverStuffStepThankYou from "./FormHandOverStuffStepThankYou";
 
 function FormHandOverStuffSteps() {
   return (
@@ -14,7 +15,7 @@ function FormHandOverStuffSteps() {
         </p>
       </section>
       <section className="formHandOverStuffSteps__singleStep">
-        <FormHandOverStuffStep3 />
+        <FormHandOverStuffStepThankYou />
       </section>
     </>
   );
